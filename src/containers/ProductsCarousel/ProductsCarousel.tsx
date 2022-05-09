6a5719a651f19d02fcd2c42ab9ec5ae78a7d0ff7
daonghia2404 <daonghia2404@gmail.com>
2022-05-09 16:37:25 +0700

    Fix UI Mobile

--- src/containers/ProductsCarousel/ProductsCarousel.tsx
+++ src/containers/ProductsCarousel/ProductsCarousel.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { navigate } from '@reach/router';
+import { useSelector } from 'react-redux';
 
 import HeaderSkew from '@/components/HeaderSkew';
 import ProductBox from '@/components/ProductBox';
 import Carousels from '@/components/Carousels';
 import { Paths } from '@/pages/routers';
 import { formatMoneyVND } from '@/utils/functions';
+import { TRootState } from '@/redux/reducers';
+import { EDeviceType } from '@/redux/reducers/ui';
 
 import { TProductsCarouselProps } from './ProductsCarousel.types';
 import './ProductsCarousel.scss';
 
 const ProductsCarousel: React.FC<TProductsCarouselProps> = ({ data = [], title }) => {
+  const window = useSelector((state: TRootState) => state.uiReducer.device.type);
+
+  const isMobile = window === EDeviceType.MOBILE;
   const splitData = [...data].splice(0, 5);
 
   const isShowCarousel = splitData.length >= 3;
@@ -29,75 +35,96 @@ const ProductsCarousel: React.FC<TProductsCarouselProps> = ({ data = [], title }
           <div className="ProductsCarousel-wrapper">
             {title && <HeaderSkew title={title} center />}
 
-            <div className="ProductsCarousel-main flex flex-wrap">
-              <div className="ProductsCarousel-main-item">
-                <ProductBox
-                  hasBg
-                  {...firstBlock}
-                  type={firstBlock.type}
-                  image={firstBlock.image}
-                  title={firstBlock.name}
-                  sale={Number(firstBlock.sale)}
-                  price={Number(firstBlock.price)}
-                  link={Paths.Product(firstBlock.id)}
-                />
-              </div>
-              <div className="ProductsCarousel-main-item">
-                <Carousels autoplay={false} dots={false}>
-                  {middleBlock.map((item) => (
-                    <div key={item.id}>
-                      <div className="ProductsCarousel-carousel-item flex flex-wrap">
-                        <div
-                          className="ProductsCarousel-carousel-item-image"
-                          onClick={(): void => handleNavigateProductDetail(item.id)}
-                        >
-                          <img src={item.image} alt="" />
-                        </div>
-                        <div className="ProductsCarousel-carousel-item-info">
-                          <div
-                            className="ProductsCarousel-carousel-item-info-title"
-                            onClick={(): void => handleNavigateProductDetail(item.id)}
-                          >
-                            {item.name}
-                          </div>
-                          <div className="ProductsCarousel-carousel-item-info-subtitle">Đối tượng sử dụng:</div>
-                          <div
-                            className="ProductsCarousel-carousel-item-info-description"
-                            // eslint-disable-next-line react/no-danger
-                            dangerouslySetInnerHTML={{
-                              __html: item.productObject?.map((obj) => obj.details)?.join(' ') || '',
-                            }}
-                          />
-                          <div className="ProductsCarousel-carousel-item-info-price">
-                            {formatMoneyVND({ amount: item.price })} VNĐ
-                          </div>
-                          <div className="ProductsCarousel-carousel-item-info-btn flex justify-center">
-                            <div
-                              className="ProductsCarousel-carousel-item-info-cta"
-                              onClick={(): void => handleNavigateProductDetail(item.id)}
-                            >
-                              Mua ngay
-                            </div>
-                          </div>
-                        </div>
-                      </div>
+            {isMobile ? (
+              <div className="ProductsCarousel-carousel-mobile">
+                <Carousels autoplay={false} slidesToShow={2}>
+                  {data.map((item) => (
+                    <div className="ProductsCarousel-carousel-mobile-item">
+                      <ProductBox
+                        {...item}
+                        hasBg
+                        type={item.type}
+                        image={item.image}
+                        title={item.name}
+                        sale={Number(item.sale)}
+                        price={Number(item.price)}
+                        link={Paths.Product(item.id)}
+                      />
                     </div>
                   ))}
                 </Carousels>
               </div>
-              <div className="ProductsCarousel-main-item">
-                <ProductBox
-                  hasBg
-                  {...lastBlock}
-                  type={lastBlock.type}
-                  image={lastBlock.image}
-                  title={lastBlock.name}
-                  sale={Number(lastBlock.sale)}
-                  price={Number(lastBlock.price)}
-                  link={Paths.Product(lastBlock.id)}
-                />
+            ) : (
+              <div className="ProductsCarousel-main flex flex-wrap">
+                <div className="ProductsCarousel-main-item">
+                  <ProductBox
+                    hasBg
+                    {...firstBlock}
+                    type={firstBlock.type}
+                    image={firstBlock.image}
+                    title={firstBlock.name}
+                    sale={Number(firstBlock.sale)}
+                    price={Number(firstBlock.price)}
+                    link={Paths.Product(firstBlock.id)}
+                  />
+                </div>
+                <div className="ProductsCarousel-main-item">
+                  <Carousels autoplay={false} dots={false}>
+                    {middleBlock.map((item) => (
+                      <div key={item.id}>
+                        <div className="ProductsCarousel-carousel-item flex flex-wrap">
+                          <div
+                            className="ProductsCarousel-carousel-item-image"
+                            onClick={(): void => handleNavigateProductDetail(item.id)}
+                          >
+                            <img src={item.image} alt="" />
+                          </div>
+                          <div className="ProductsCarousel-carousel-item-info">
+                            <div
+                              className="ProductsCarousel-carousel-item-info-title"
+                              onClick={(): void => handleNavigateProductDetail(item.id)}
+                            >
+                              {item.name}
+                            </div>
+                            <div className="ProductsCarousel-carousel-item-info-subtitle">Đối tượng sử dụng:</div>
+                            <div
+                              className="ProductsCarousel-carousel-item-info-description"
+                              // eslint-disable-next-line react/no-danger
+                              dangerouslySetInnerHTML={{
+                                __html: item.productObject?.map((obj) => obj.details)?.join(' ') || '',
+                              }}
+                            />
+                            <div className="ProductsCarousel-carousel-item-info-price">
+                              {formatMoneyVND({ amount: item.price })} VNĐ
+                            </div>
+                            <div className="ProductsCarousel-carousel-item-info-btn flex justify-center">
+                              <div
+                                className="ProductsCarousel-carousel-item-info-cta"
+                                onClick={(): void => handleNavigateProductDetail(item.id)}
+                              >
+                                Mua ngay
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+                  </Carousels>
+                </div>
+                <div className="ProductsCarousel-main-item">
+                  <ProductBox
+                    hasBg
+                    {...lastBlock}
+                    type={lastBlock.type}
+                    image={lastBlock.image}
+                    title={lastBlock.name}
+                    sale={Number(lastBlock.sale)}
+                    price={Number(lastBlock.price)}
+                    link={Paths.Product(lastBlock.id)}
+                  />
+                </div>
               </div>
-            </div>
+            )}
           </div>
         )}
       </div>
--- src/containers/ReviewsCarousel/ReviewsCarousel.tsx
+++ src/containers/ReviewsCarousel/ReviewsCarousel.tsx
@@ -1,12 +1,56 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import HeaderSkew from '@/components/HeaderSkew';
-import ImageReview from '@/assets/images/image-review.png';
+import ImageReview1 from '@/assets/images/image-review-1.png';
+import ImageReview2 from '@/assets/images/image-review-2.png';
+import ImageReview3 from '@/assets/images/image-review-3.png';
+import ImageReview4 from '@/assets/images/image-review-4.png';
+import ImageReview5 from '@/assets/images/image-review-5.jpeg';
 import Carousels from '@/components/Carousels';
+import { TRootState } from '@/redux/reducers';
 
 import './ReviewsCarousel.scss';
 
 const ReviewsCarousel: React.FC = () => {
+  const window = useSelector((state: TRootState) => state.uiReducer.device.width);
+
+  const isMobile = window <= 768;
+
+  const dataReviews = [
+    {
+      name: 'Châu Bùi',
+      description: 'Influencer',
+      content: 'Supplements Việt Nam luôn là người bạn đồng hành đáng tin cậy trong suốt quá trình luyện tập của tôi.',
+      image: ImageReview1,
+    },
+    {
+      name: 'Nguyễn H. Nam',
+      description: 'Á Vương Mister Việt Nam 2019',
+      content: 'Một sản phẩm không thể thiếu nếu bạn thật sự quan tâm tới sức khỏe của mình.',
+      image: ImageReview2,
+    },
+    {
+      name: 'Trà My',
+      description: 'Influencer',
+      content: 'Tôi chọn sử dụng VNSupplements mỗi ngày để bổ sung và cân bằng các vitamin cần thiết cho cơ thể.',
+      image: ImageReview3,
+    },
+    {
+      name: 'Lê Thành Công',
+      description: 'Hot Gymer',
+      content: 'Sản phẩm chất lượng cao, được kiểm định từ Bộ Y Tế nên tôi rất yên tâm khi sử dụng.',
+      image: ImageReview4,
+    },
+    {
+      name: 'Shin Phạm',
+      description: 'Hot Gymer',
+      content:
+        'Sử dụng sản phẩm bổ sung của Vietnam’s Supplements mỗi ngày luôn là yếu tố quan trọng trong cuộc sống luyện tập của tôi.',
+      image: ImageReview5,
+    },
+  ];
+
   return (
     <div className="ReviewsCarousel">
       <div className="container">
@@ -14,39 +58,20 @@ const ReviewsCarousel: React.FC = () => {
           <HeaderSkew title="Chuyên gia cùng" center />
 
           <div className="ReviewsCarousel-main">
-            <Carousels autoplay dots={false}>
-              {[1, 2].map((item) => (
-                <div key={item}>
-                  <div className="ReviewsCarousel-carousel flex justify-between flex-wrap">
-                    <div className="ReviewsCarousel-carousel-item">
-                      <div className="ReviewsCarousel-carousel-item-image">
-                        <img src={ImageReview} alt="" />
-                      </div>
-                      <div className="ReviewsCarousel-carousel-item-info">
-                        <div className="ReviewsCarousel-carousel-item-info-header">
-                          <div className="ReviewsCarousel-carousel-item-info-header-name">Châu Bùi</div>
-                          <div className="ReviewsCarousel-carousel-item-info-header-description">Influencer</div>
-                        </div>
-                        <div className="ReviewsCarousel-carousel-item-info-quote">
-                          Supplements Việt Nam luôn là người bạn đồng hành đáng tin cậy trong suốt quá trình luyện tập
-                          của tôi.
-                        </div>
-                      </div>
+            <Carousels autoplay dots={isMobile} slidesToShow={isMobile ? 1 : 2}>
+              {dataReviews.map((item, index) => (
+                // eslint-disable-next-line react/no-array-index-key
+                <div key={index} className="ReviewsCarousel-carousel flex justify-between flex-wrap">
+                  <div className="ReviewsCarousel-carousel-item">
+                    <div className="ReviewsCarousel-carousel-item-image">
+                      <img src={item.image} alt="" />
                     </div>
-                    <div className="ReviewsCarousel-carousel-item">
-                      <div className="ReviewsCarousel-carousel-item-image">
-                        <img src={ImageReview} alt="" />
+                    <div className="ReviewsCarousel-carousel-item-info">
+                      <div className="ReviewsCarousel-carousel-item-info-header">
+                        <div className="ReviewsCarousel-carousel-item-info-header-name">{item.name}</div>
+                        <div className="ReviewsCarousel-carousel-item-info-header-description">{item.description}</div>
                       </div>
-                      <div className="ReviewsCarousel-carousel-item-info">
-                        <div className="ReviewsCarousel-carousel-item-info-header">
-                          <div className="ReviewsCarousel-carousel-item-info-header-name">Châu Bùi</div>
-                          <div className="ReviewsCarousel-carousel-item-info-header-description">Influencer</div>
-                        </div>
-                        <div className="ReviewsCarousel-carousel-item-info-quote">
-                          Supplements Việt Nam luôn là người bạn đồng hành đáng tin cậy trong suốt quá trình luyện tập
-                          của tôi.
-                        </div>
-                      </div>
+                      <div className="ReviewsCarousel-carousel-item-info-quote">{item.content}</div>
                     </div>
                   </div>
                 </div>
